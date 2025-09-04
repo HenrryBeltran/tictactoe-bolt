a4abfed6c79a6a9a644622a9e43db326c4cb4b9d
Henrry <henrry26bt@gmail.com>
2025-09-03 22:09:47 -0500

    Add the play pages and add some basic ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<main>
-  <h1>Tic Tac Toe</h1>
-  <h2>Bolt Edition</h2>
+<main class="relative flex min-h-lvh items-center justify-center">
+  <div class="absolute top-0 right-0">
+    <button class="mt-6 mr-6 rounded-full bg-neutral-700 px-3 py-0.75 text-white">Config</button>
+  </div>
+  <div class="mx-6 w-full max-w-lg border">
+    <header class="border border-red-400">
+      <h1 class="text-2xl font-bold text-neutral-900">Tic Tac Toe</h1>
+      <h2 class="font-bold text-neutral-700">Bolt Edition</h2>
+    </header>
+    <div>
+      <div class="flex flex-col gap-4">
+        <a href="/pvp">Player vs Player</a>
+        <a href="/pvc">Player vs Computer</a>
+        <a href="/instructions">Instructions</a>
+      </div>
+    </div>
+  </div>
 </main>
